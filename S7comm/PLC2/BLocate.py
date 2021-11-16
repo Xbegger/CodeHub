27bb74062d800fad3d2355a7dd2d88d3a0653a9c
Xbegger <2421496901@qq.com>
2021-11-16 20:53:45 +0800

    11.16

---
+++ S7comm/PLC2/BLocate.py
@@ -0,0 +1,95 @@
+from struct import pack
+from MyS7Client import MyS7Client
+from BaseClass import Base
+
+from icssploit.protocols.cotp import *
+from icssploit.protocols.s7comm import *
+
+import threading
+
+
+class BLocate(Base):
+
+    def __init__(self, candidatePackets, handleCrush):
+        super(BLocate, self).__init__()
+
+        self.candidatePackets = candidatePackets
+        self.__s7Client = handleCrush.getS7Client()
+        self.__handleCrush = handleCrush
+
+
+    def locate(self):
+        self.logger.info("Start binary search crush")
+        candidatePackets = self.candidatePackets
+
+        self.__s7Client.connect()
+        crushListenThread = threading.Thread(target=self.__s7Client.crushListen)
+        crushListenThread.start()
+        self.__s7Client.crush.clear()
+        while(candidatePackets):
+            chosenPackets, leftPackets, length = branchPackets(candidatePackets)
+            # add last test packet
+            chosenPackets.append(chosenPackets[0])
+
+            self.__s7Client.run.set()
+            crush = False
+
+            # self.__s7Client.crush = False
+            msg = "chosenPacket from %d to %d ; leftPackets from %d to %d." %(0, length/2, length/2, length)
+            self.logger.info(msg)
+            
+            for packet in chosenPackets:
+                self.__s7Client.send_s7_packet(packet)
+                time.sleep(5)
+                if(self.__s7Client.crush.isSet() == True):
+                    # 主动探测确认奔溃
+                    crush = not self.__s7Client.onlinePLC()
+                    if(crush == True):
+                        # self.logger.info("find a crush")
+                        break
+                    else:
+                        self.__s7Client.run.set()
+    
+
+            
+            time.sleep(3)
+            # crush = not self.__s7Client.onlinePLC()
+            if(crush == True):
+                self.logger.info("find a crush")
+                self.__handleCrush.handleCrush()
+                # time.sleep(3)
+                self.__s7Client.connect()
+
+                candidatePackets = chosenPackets
+            else:
+                candidatePackets = leftPackets
+
+            if(isCurshPacket(candidatePackets) == True):
+                break
+        
+        crushPacket = raw(candidatePackets[0]).hex()
+        self.logger.info("Crush Packet is %s" % (crushPacket))
+        return candidatePackets[0]
+        
+
+
+
+
+
+
+
+def branchPackets(candidatePackets):
+    length = len(candidatePackets)
+    half = length // 2
+    chosenPackets = candidatePackets[0 : half]
+    leftPackets = candidatePackets[half : ]
+    return chosenPackets, leftPackets,length 
+
+
+
+def isCurshPacket( packets):
+    length = len(packets)
+    if(length == 1):
+        return True
+
+    return False
---
+++ S7comm/PLC2/MIPlugin.py
@@ -0,0 +1,101 @@
+from miio.miioprotocol import MiIOProtocol
+from typing import Any
+
+
+class MIPlugin():
+
+    retry_count = 3
+    timeout = 5
+    def __init__(
+        self,
+        ip: str = None,
+        token: str = None,
+        start_id: int = 0,
+        debug: int = 0,
+        lazy_discover: bool = True,
+        timeout: int = None,
+    ) -> None:
+        self.ip = ip
+        self.token = token
+        timeout = timeout if timeout is not None else self.timeout
+        self._protocol = MiIOProtocol(
+            ip, token, start_id, debug, lazy_discover, timeout
+        )
+
+    def send(
+        self,
+        command: str,
+        parameters: Any = None,
+        retry_count: int = None,
+        *,
+        extra_parameters=None,
+    ) -> Any:
+        """Send a command to the device.
+
+        Basic format of the request:
+        {"id": 1234, "method": command, "parameters": parameters}
+
+        `extra_parameters` allows passing elements to the top-level of the request.
+        This is necessary for some devices, such as gateway devices, which expect
+        the sub-device identifier to be on the top-level.
+
+        :param str command: Command to send
+        :param dict parameters: Parameters to send
+        :param int retry_count: How many times to retry on error
+        :param dict extra_parameters: Extra top-level parameters
+        """
+        retry_count = retry_count if retry_count is not None else self.retry_count
+        return self._protocol.send(
+            command, parameters, retry_count, extra_parameters=extra_parameters
+        )
+
+    def on(self):
+        """set the plugin on
+
+            :return bool True: switch on the plugin sucess
+            False: switch on the plugin failed
+        """
+        msg = {'did':'MYDID','siid':2,'piid':1,'value':True}
+        recv = self.set_properties([msg])
+        if self.power_status() == True:
+            return True
+        else:
+            return False
+    
+    def off(self):
+        """set the plugin off
+
+            :return bool True: switch off the plugin sucess
+                        False: switch off the plugin failed
+        """
+        msg = {'did':'MYDID','siid':2,'piid':1,'value':False}
+        recv = self.set_properties([msg])
+        if self.power_status() == False:
+            return True
+        else:
+            return False
+
+
+    def power_status(self):
+        """get a status of plugin
+
+            :return bool True: the plugin is on; 
+                        False: the plugin is off;
+        """
+        msg = {'did':'MYDID','siid':2,'piid':1}
+        recv = self.get_properties(msg)
+        return recv[0]['value']
+
+    def set_properties(self, parameters):
+        """send a set_properties command
+
+        :param dict parameters:Parameters to send
+        """
+        return self.send("set_properties", parameters)
+
+    def get_properties(self, parameters):
+        """send a get_properties command
+
+        :param dict parameters:Parameters to send
+        """
+        return self.send("get_properties", [parameters])
